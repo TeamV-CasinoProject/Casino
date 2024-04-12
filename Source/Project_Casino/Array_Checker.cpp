5af6f308eca81a4c0e5dcf7d45f817de524a7a50
mireuden <a01084893912@gmail.com>
2024-04-12 14:17:35 +0900

    04.12
    체크

---
+++ Source/Project_Casino/Array_Checker.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Array_Checker.h"
+
+// Sets default values
+AArray_Checker::AArray_Checker()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AArray_Checker::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AArray_Checker::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
