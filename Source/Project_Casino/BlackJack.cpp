51c2bc17bb8d03dcd81a36ab1608678d38883f0b
siruduk <124989831+siruduk@users.noreply.github.com>
2024-04-15 02:52:27 +0900

    24.04.15
    콘솔창에서 Hit, Stay 구현 완료

--- Source/Project_Casino/BlackJack.cpp
+++ Source/Project_Casino/BlackJack.cpp
@@ -9,6 +9,14 @@ void ABlackJack::InitGame()
 {
 	//설명 띄우기
 	//플레이어 수 설정 (혼자 여러 덱을 사용하게 개발할 경우 추가)
+	//test용
+	PlayerCount = 3;//설정하는부분 ui로 만들기
+
+	//플레이어 수 + 1 만큼 생성
+	for (int i = 0; i < PlayerCount + 1; i++)
+	{
+		PlayerList.Push(PlayerInfo());
+	}
 	SetDeck();
 }
 
@@ -21,43 +29,129 @@ void ABlackJack::SetDeck()
 			{
 				Card c(i, k);
 				Deck.Push(Card(i, k));
-				UE_LOG(LogTemp, Warning, TEXT("%d  %d"), c.Suit, c.Num);
 			}
 	
-
 	for (int i = 0; i < 52*4 - 1; i++)
 	{
-		// `i <= j < n`이 되도록 난수 `j`를 생성하고
-		// 인덱스 `j`에 있는 요소를 해당 요소로 교체합니다.
-		// 현재 인덱스 `i`에 존재
 		int j = i + rand() % (52*4 - i);
 		std::swap(Deck[i], Deck[j]);
 	}
 }
 
-void ABlackJack::Hit()
+void ABlackJack::Bet()
 {
-	UE_LOG(LogTemp, Warning, TEXT("%d  %d"), Deck[PlayerPoint].Suit, Deck[PlayerPoint].Num);
-	p.Hand.Push(Deck[PlayerPoint++]);
-	p.CalcSum();
+	//배팅 금액 정하기 자리 별로
+}
+
+void ABlackJack::Double()
+{
+	//내가 받을 두장의 카드가 더블이라고 예상되면 배팅
+}
+
+//라운드 초기화
+void ABlackJack::InitRound()
+{
+	for (PlayerPoint = 0; PlayerPoint < PlayerCount; PlayerPoint++)
+		Hit();
+	Hit();//카드 보여줄때 뒷면으로
+
+	for (PlayerPoint = 0; PlayerPoint < PlayerCount; PlayerPoint++)
+		Hit();
+	Hit();//얘는 앞면으로
+
+	for (PlayerPoint = 0; PlayerPoint < PlayerCount; PlayerPoint++)
+	{
+		if(PlayerList[PlayerPoint].Hand[0].Num == PlayerList[PlayerPoint].Hand[1].Num)
+			UE_LOG(LogTemp, Warning, TEXT("%d Player  Double"), PlayerPoint);
+	}
+	PlayerPoint = 0;
+
 }
 
 void ABlackJack::DoubleDown()
 {
+	//배팅금액 두배
 	Hit();
-	//배팅금액 낸만큼 더내기
-	//Ai에게 턴넘기기
+	Stay();
 }
 
-//void ABlackJack::Hit(PlayerInfo p)
-//{
-//	UE_LOG(LogTemp, Warning, TEXT("%s  %d"), Deck[PlayerPoint].Suit, Deck[PlayerPoint].Num);
-//	p.Hand.Push(Deck[PlayerPoint++]);
-//	p.CalcSum();
-//}
+void ABlackJack::Calc()
+{
+	if (!IsDealerTurn)
+	{
+		if (PlayerList[PlayerPoint].CalcSum() > 21)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("%d Player Burst"), PlayerPoint);
+			PlayerList[PlayerPoint].Sum = 0;
+			Stay();
+		}
+	}
+	else
+		if (PlayerList[PlayerCount].CalcSum() < 16)
+			Hit();
+		else
+			RoundEnd();
+}
 
- void PlayerInfo::CalcSum() {
-	Sum = 0;
+
+void ABlackJack::RoundEnd()
+{
+	UE_LOG(LogTemp, Warning, TEXT("RoundEnd"));	
+
+	UE_LOG(LogTemp, Warning, TEXT("Dealer %d"), PlayerList[PlayerCount].Sum);
+	int Dealer = PlayerList[PlayerCount].Sum;
+	for (int i = 0; i < PlayerCount; i++)
+	{
+		if (PlayerList[i].Sum > Dealer)
+		{
+			if (PlayerList[i].Sum == 21)
+			{
+				UE_LOG(LogTemp, Warning, TEXT("%d Player win BlackJack * 2.5"), i);
+			}
+			else
+				UE_LOG(LogTemp, Warning, TEXT("%d Player win * 2"), i);
+
+		}
+		else if (PlayerList[i].Sum == Dealer)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("%d Player Draw"), i);//건만큼
+		}
+		else
+			UE_LOG(LogTemp, Warning, TEXT("%d Player Loser"), i);//건만큼
+		if (Dealer == 21)
+			return;//인슈어 * 2배
+	}
+}
+
+void ABlackJack::Stay()
+{
+	UE_LOG(LogTemp, Warning, TEXT("%d Player End"), PlayerPoint);
+	PlayerPoint++;
+	if (PlayerCount == PlayerPoint)
+	{
+		IsDealerTurn = true;
+		Hit();
+	}
+	else if (PlayerCount > PlayerPoint)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%d Player Turn	  Sum : %d"), PlayerPoint,PlayerList[PlayerPoint].Sum);
+	}
+}
+
+void ABlackJack::Insurance()
+{
+}
+
+void ABlackJack::Hit()
+{
+	UE_LOG(LogTemp, Warning, TEXT("%d Player  %d"), PlayerPoint, Deck[DeckPoint].Num);
+	PlayerList[PlayerPoint].Hand.Push(Deck[DeckPoint++]);
+	Calc();
+}
+
+ int PlayerInfo::CalcSum() {
+	 int AceCount = 0;
+	 Sum = 0;
 	for (auto It = Hand.begin(); It != Hand.end(); ++It)
 	{
 		Card Element = *It;
@@ -70,6 +164,12 @@ void ABlackJack::DoubleDown()
 			AceCount++;
 			Sum += 10;
 		}
+		if (Sum > 21 && AceCount > 0)
+		{
+			AceCount--;
+			Sum -= 10;
+		}
 	}
-	UE_LOG(LogTemp, Warning, TEXT("sum %d"), Sum);
+	UE_LOG(LogTemp, Warning, TEXT("Player sum %d"),Sum); //UI에 연결하기
+	return Sum;
 }
--- Source/Project_Casino/BlackJack.h
+++ Source/Project_Casino/BlackJack.h
@@ -17,9 +17,12 @@ public:
 	TArray<Card> Hand;
 
 	int Sum;
-	int AceCount;
+	int First;
+	int Second;
 	void InitHand();
-	void CalcSum();
+	int CalcSum();
+	int Coin = 0;// 배팅한 금액
+	int InsuranceCoin = 0; //보험금
 private:
 };
 
@@ -28,22 +31,35 @@ class PROJECT_CASINO_API ABlackJack : public AGameModeBase
 {
 	GENERATED_BODY()
 public:
-	void InitGame(); // 게임 시작
 	UFUNCTION(BlueprintCallable, Category = "BlackJack")
+	void InitGame(); // 게임 시작
 	void SetDeck(); // 게임 시작하고 초기 덱 준비
-	void InitRound(); // 딜러와 플레이어의 초기 카드 준비
+	UFUNCTION(BlueprintCallable, Category = "BlackJack")
 	void Bet(); // 초기 배팅금액 설정
 	UFUNCTION(BlueprintCallable, Category = "BlackJack")
-	void Hit();
-	void DoubleDown();
-	void Hit(PlayerInfo); //매개변수로 딜러, 플레이어를 받아 딜러/플레이어에게 카드 한장 줌
+	void Double();
+	UFUNCTION(BlueprintCallable, Category = "BlackJack")
+	void InitRound(); // 딜러와 플레이어의 초기 카드 준비
+	UFUNCTION(BlueprintCallable, Category = "BlackJack")
+	void Hit();// 카드 한장 더
+	UFUNCTION(BlueprintCallable, Category = "BlackJack")
+	void Stay();// 턴종료
+	UFUNCTION(BlueprintCallable, Category = "BlackJack")
+	void Insurance(); 
+	//딜러가 블랙잭일거라 생각될때 배팅금액의 절반까지 보험으로 추가지불 블랙잭이면 보험금의 두배 아니면 증발 첫카드가 A아니여도 가능하게 룰 수정
+	UFUNCTION(BlueprintCallable, Category = "BlackJack")
+	void DoubleDown();// 다음 카드를 받고 턴 종료 이기면 2.5배
+	UFUNCTION(BlueprintCallable, Category = "BlackJack")
+	void Calc();//버스트인지, 딜러의 턴이 끝났는지 확인
 
+	void Dealer();//딜러의 턴
+	void RoundEnd();//딜러의 턴이끝나고 배팅금액 정산
 public:
 	TArray<Card> Deck; //덱
-	TArray<PlayerInfo> PlayerList; //혼자 1~3플레이어 하게되면 사용 0 : 딜러, 1~3 : 플레이어
+	TArray<PlayerInfo> PlayerList; // 0~2 : 플레이어 마지막 : 딜러
 
-	PlayerInfo p = PlayerInfo();
+	int PlayerCount;//플레이어의 수
 	int DeckPoint; //카드를 얼마나 썻나?
 	int PlayerPoint; //누구턴인지 확인
-	int Coin;// 배팅한 금액
+	bool IsDealerTurn = false;
 };
